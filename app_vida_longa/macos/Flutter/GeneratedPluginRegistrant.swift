d5a4a6589d91eae4f952acf52adcb17a716f7c97
giovanialves <giovani.alves.glv@gmail.com>
2025-09-16 11:09:42 -0300

    chore: more updates for andriod build

--- app_vida_longa/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app_vida_longa/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -17,13 +17,13 @@ import google_sign_in_ios
 import in_app_purchase_storekit
 import path_provider_foundation
 import share_plus
-import sqflite_darwin
+import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
