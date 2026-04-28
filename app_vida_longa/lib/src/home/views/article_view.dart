b9bb8f5bd3238b205ef6b055a193cb87681b6c92
Andreyna Carvalho <andreyna.m.carvalho@gmail.com>
2026-04-28 08:18:52 -0400

    fix: buttons correction

--- app_vida_longa/lib/src/home/views/article_view.dart
+++ app_vida_longa/lib/src/home/views/article_view.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:ui' show FlutterView;
 
 import 'package:app_vida_longa/core/helpers/app_helper.dart';
 import 'package:app_vida_longa/core/services/articles_service.dart';
@@ -7,7 +8,6 @@ import 'package:app_vida_longa/core/services/user_service.dart';
 import 'package:app_vida_longa/domain/contants/app_colors.dart';
 import 'package:app_vida_longa/domain/enums/user_service_status_enum.dart';
 import 'package:app_vida_longa/domain/models/article_model.dart';
-import 'package:app_vida_longa/shared/widgets/custom_scaffold.dart';
 import 'package:app_vida_longa/shared/widgets/default_app_bar.dart';
 import 'package:app_vida_longa/shared/widgets/default_text.dart';
 import 'package:app_vida_longa/src/core/navigation_controller.dart';
@@ -46,14 +46,12 @@ class _ArticleViewState extends State<ArticleView> {
       RegExp(r'youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)'),
       RegExp(r'youtu\.be\/([a-zA-Z0-9_-]+)'),
     ];
-
     for (final pattern in patterns) {
       final match = pattern.firstMatch(html);
       if (match != null && match.groupCount >= 1) {
         return match.group(1)!;
       }
     }
-
     return '';
   }
 
@@ -66,6 +64,7 @@ class _ArticleViewState extends State<ArticleView> {
     } else {
       _currentlyArticle = ArticleService.currentlyArticle;
     }
+
     isFavorited = _favoritesService.favoritesIds.contains(
       _currentlyArticle.uuid,
     );
@@ -86,7 +85,6 @@ class _ArticleViewState extends State<ArticleView> {
         );
       } else if (item.type == "video") {
         final videoId = _extractYoutubeId(item.content);
-
         if (videoId.isNotEmpty) {
           widgets.add(
             Padding(
@@ -113,102 +111,90 @@ class _ArticleViewState extends State<ArticleView> {
         );
       }
     }
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return CustomAppScaffold(
-      appBar: const DefaultAppBar(title: "Artigo", isWithBackButton: true),
-      //  appBar: AppBar(
-      //   centerTitle: true,
-      //   backgroundColor: AppColors.white,
-      //   title: const DefaultText(
-      //     "Artigo",
-      //     fontSize: 20,
-      //     fontWeight: FontWeight.w300,
-      //   ),
-      //   leading: IconButton(
-      //     icon: const Icon(Icons.arrow_back_ios, color: AppColors.matterhorn),
-      //     onPressed: () {
-      //       Navigator.pop(context);
-      //     },
-      //   ),
-      // ),
-      hasScrollView: true,
-      body: Column(
-        children: [
-          DefaultText(
-            _currentlyArticle.title,
-            fontSize: 24,
-            fontWeight: FontWeight.w700,
-            maxLines: 4,
-            textAlign: TextAlign.center,
-          ),
-          ...widgets,
-        ],
-      ),
-      bottomNavigationBar: optionsBottomBar(),
-    );
-  }
+    final isLoggedOut =
+        UserService.instance.status == UserServiceStatusEnum.loggedOut;
+    final FlutterView view = View.of(context);
+    final double devicePixelRatio = view.devicePixelRatio;
+    final double bottomInset = view.padding.bottom / devicePixelRatio;
 
-  Widget optionsBottomBar() {
-    return BottomAppBar(
-      clipBehavior: Clip.antiAlias,
-      height: 60,
-      shape: const CircularNotchedRectangle(),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          IconButton(
-            onPressed: () {
-              NavigationController.push("/app/home/comments");
-            },
-            icon: const Icon(Icons.comment_rounded, size: 30),
+    return Scaffold(
+      appBar: const DefaultAppBar(title: "Artigo", isWithBackButton: true),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 8),
+            DefaultText(
+              _currentlyArticle.title,
+              fontSize: 24,
+              fontWeight: FontWeight.w700,
+              maxLines: 4,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 16),
+            ...widgets,
+            const SizedBox(height: 30),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        color: AppColors.white,
+        padding: EdgeInsets.only(bottom: bottomInset > 0 ? bottomInset : 20.0),
+        child: SizedBox(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () =>
+                    NavigationController.push("/app/home/comments"),
+                icon: const Icon(Icons.comment_rounded, size: 30),
+              ),
+              IconButton(
+                icon: const Icon(Icons.zoom_out, size: 30),
+                onPressed: () => _handleFontSize(-2),
+              ),
+              IconButton(
+                icon: const Icon(Icons.zoom_in, size: 30),
+                onPressed: () => _handleFontSize(2),
+              ),
+              isLoggedOut
+                  ? IconButton(
+                      onPressed: () => AppHelper.displayAlertInfo(
+                        "Você precisa estar logado para favoritar um artigo!",
+                      ),
+                      icon: const FaIcon(FontAwesomeIcons.heartCircleXmark),
+                    )
+                  : IconButton(
+                      icon: isFavorited
+                          ? const FaIcon(
+                              FontAwesomeIcons.solidHeart,
+                              color: AppColors.secondary,
+                            )
+                          : const FaIcon(FontAwesomeIcons.heart),
+                      onPressed: () {
+                        if (isFavorited) {
+                          _favoritesService.remove(_currentlyArticle.uuid);
+                        } else {
+                          _favoritesService.add(_currentlyArticle.uuid);
+                        }
+                        setState(() => isFavorited = !isFavorited);
+                      },
+                    ),
+            ],
           ),
-          IconButton(
-            icon: const Icon(Icons.zoom_out, size: 30),
-            onPressed: () => _handleFontSize(-2),
-          ),
-          IconButton(
-            icon: const Icon(Icons.zoom_in, size: 30),
-            onPressed: () => _handleFontSize(2),
-          ),
-          UserService.instance.status == UserServiceStatusEnum.loggedOut
-              ? IconButton(
-                  onPressed: () {
-                    AppHelper.displayAlertInfo(
-                      "Você precisa estar logado para favoritar um artigo!",
-                    );
-                  },
-                  icon: const FaIcon(FontAwesomeIcons.heartCircleXmark),
-                )
-              : IconButton(
-                  icon: isFavorited
-                      ? const FaIcon(
-                          FontAwesomeIcons.solidHeart,
-                          color: AppColors.secondary,
-                        )
-                      : const FaIcon(FontAwesomeIcons.heart),
-                  onPressed: () {
-                    if (isFavorited) {
-                      _favoritesService.remove(_currentlyArticle.uuid);
-                    } else {
-                      _favoritesService.add(_currentlyArticle.uuid);
-                    }
-                    setState(() {
-                      isFavorited = !isFavorited;
-                    });
-                  },
-                ),
-        ],
+        ),
       ),
     );
   }
 
   void _handleFontSize(double size) {
-    fontSize = fontSize + size;
+    fontSize += size;
     _streamControllerFontSize.add(fontSize);
   }
 }
